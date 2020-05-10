c5c6b5b070bacfa6c9bfc7c0eaede19ca21c8760
kisstar <dwh.chn@foxmail.com>
2020-05-10 19:30:20 +0800

    feat: 使用异常捕获边界处理加载出错

--- src/App.js
+++ src/App.js
@@ -2,12 +2,39 @@ import React, { lazy, Suspense } from 'react';
 
 const LazyAbout = lazy(() => import(/* webpackChunkName: "about" */ './About'));
 
+class ErrorBoundary extends React.Component {
+  state = {
+    hasError: false,
+  };
+
+  static getDerivedStateFromError(_error) {
+    // 更新 state 使下一次渲染能够显示降级后的 UI
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // 可以在此将错误日志上报给服务器
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // 自定义降级后的 UI 并渲染
+      return <div>Error</div>;
+    }
+
+    return this.props.children;
+  }
+}
+
 class App extends React.Component {
   render() {
     return (
-      <Suspense fallback={<div>Loading</div>}>
-        <LazyAbout />
-      </Suspense>
+      <ErrorBoundary>
+        <Suspense fallback={<div>Loading</div>}>
+          <LazyAbout />
+        </Suspense>
+      </ErrorBoundary>
     );
   }
 }
