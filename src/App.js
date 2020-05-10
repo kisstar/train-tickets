235def1e970b225cc30c640e5136db9572cd06ba
kisstar <dwh.chn@foxmail.com>
2020-05-10 21:56:54 +0800

    feat: 使用 class 实现计数器组件

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
 import React from 'react';
 
-const Foo = React.memo(function Foo(props) {
-  console.log('Foo render');
-  return <div>{props.person.age}</div>;
-});
-
 class App extends React.Component {
   state = {
     count: 0,
-    person: {
-      age: 1,
-    },
   };
 
-  callback = () => {};
-
   render() {
-    const { count, person } = this.state;
+    const { count } = this.state;
 
     return (
       <>
-        <button
-          onClick={() => {
-            person.age++;
-            this.setState({ count: count + 1 });
-          }}
-        >
-          Add
+        <button onClick={() => this.setState({ count: count + 1 })}>
+          Click {count}
         </button>
-        <Foo person={person} cb={this.callback} />
       </>
     );
   }
