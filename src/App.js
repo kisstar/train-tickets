263a87036026249e81bd082a9354a8c40e10a884
kisstar <dwh.chn@foxmail.com>
2020-05-10 22:45:51 +0800

    feat: 基于 class 组件监听 window 的 resize 事件

--- src/App.js
+++ src/App.js
@@ -3,23 +3,43 @@ import React from 'react';
 class App extends React.Component {
   state = {
     count: 0,
+    size: {
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+    },
+  };
+
+  onSize = () => {
+    this.setState({
+      size: {
+        width: document.documentElement.clientWidth,
+        height: document.documentElement.clientHeight,
+      },
+    });
   };
 
   componentDidMount() {
     document.title = this.state.count;
+
+    window.addEventListener('resize', this.onSize, false);
   }
 
   componentDidUpdate() {
     document.title = this.state.count;
   }
 
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onSize, false);
+  }
+
   render() {
-    const { count } = this.state;
+    const { count, size } = this.state;
 
     return (
       <>
         <button onClick={() => this.setState({ count: count + 1 })}>
           Click {count}
+          Size {size.width}x{size.height}
         </button>
       </>
     );
