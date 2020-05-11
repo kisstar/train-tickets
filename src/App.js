b4f740535a1571478bc39a02ed050a2902735bbc
kisstar <dwh.chn@foxmail.com>
2020-05-11 21:40:56 +0800

    feat: Context Hooks 的基础使用

--- src/App.js
+++ src/App.js
@@ -1,60 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, createContext, useContext } from 'react';
+
+const CountContext = createContext();
+
+class Foo extends React.Component {
+  render() {
+    return (
+      <CountContext.Consumer>
+        {(count) => <h1>{count}</h1>}
+      </CountContext.Consumer>
+    );
+  }
+}
+
+class Bar extends React.Component {
+  static contextType = CountContext;
+
+  render() {
+    return <h1>{this.context}</h1>;
+  }
+}
+
+function Counter() {
+  const count = useContext(CountContext);
+  return <h1>{count}</h1>;
+}
 
 function App() {
   const [count, setCount] = useState(0);
-  const [size, setSize] = useState({
-    width: document.documentElement.clientWidth,
-    height: document.documentElement.clientHeight,
-  });
-  const onSize = () =>
-    setSize({
-      width: document.documentElement.clientWidth,
-      height: document.documentElement.clientHeight,
-    });
-
-  const onClick = () => {
-    console.log('click');
-  };
-
-  useEffect(() => {
-    document.title = count;
-  });
-
-  useEffect(() => {
-    window.addEventListener('resize', onSize, false);
-
-    return () => {
-      window.removeEventListener('resize', onSize, false);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  useEffect(() => {
-    console.log(`count: ${count}`);
-  }, [count]); // 只在 count 发生改变时执行
-
-  useEffect(() => {
-    document.querySelector('#size').addEventListener('click', onClick, false);
-
-    return () => {
-      document
-        .querySelector('#size')
-        .removeEventListener('click', onClick, false);
-    };
-  });
 
   return (
     <>
       <button onClick={() => setCount(count + 1)}>Click {count}</button>
-      {count & 2 ? (
-        <span id='size'>
-          Size {size.width}x{size.height}
-        </span>
-      ) : (
-        <p id='size'>
-          Size {size.width}x{size.height}
-        </p>
-      )}
+      <CountContext.Provider value={count}>
+        <Foo />
+        <Bar />
+        <Counter />
+      </CountContext.Provider>
     </>
   );
 }
