512c0a3e02e470a30bbf305a349f4f31ed842eba
kisstar <dwh.chn@foxmail.com>
2020-05-11 22:06:23 +0800

    feat: useCallback 的基础使用

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, memo, useCallback } from 'react';
 
-function Counter({ count }) {
-  return <h1>{count}</h1>;
-}
+const Counter = memo(function Counter({ count, onClick }) {
+  console.log('Counter render');
+  return <h1 onClick={onClick}>{count}</h1>;
+});
 
 function App() {
   const [count, setCount] = useState(0);
+  const [clickCount, setClickCount] = useState(0);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const double = useMemo(() => count * 2, [count === 3]);
 
-  /*
-    // 一个 useMemo 可以依赖另一个 useMemo 的返回值
-    // 但注意不要循环依赖，以免程序崩溃
-    const half = useMemo(() => double / 2, [double]);
-  */
+  // useCallback(fn, deps) 相当于 useMemo(() => fn, deps)
+  const onClick = useCallback(() => {
+    // setClickCount((clickCount) => clickCount + 1); // 使用此种方式更新 clickCount，则无需将 clickCount 作为依赖
+    setClickCount(clickCount + 1);
+    console.log('click');
+  }, [clickCount]);
 
   return (
     <>
       <button onClick={() => setCount(count + 1)}>
         Click {count}, Double {double}
       </button>
-      <Counter count={count} />
+      <Counter count={double} onClick={onClick} />
     </>
   );
 }
