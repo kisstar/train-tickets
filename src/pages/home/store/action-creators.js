8770bd5bbeea059f77a1b20ffc02ffe8601eecc0
kisstar <dwh.chn@foxmail.com>
2020-05-13 22:05:59 +0800

    feat: 数据结构与模块设计

---
+++ src/pages/home/store/action-creators.js
@@ -0,0 +1,96 @@
+import * as actionTypes from './action-types';
+
+export function setFrom(city) {
+  return {
+    type: actionTypes.SET_FROM,
+    payload: city,
+  };
+}
+
+export function setTo(city) {
+  return {
+    type: actionTypes.SET_TO,
+    payload: city,
+  };
+}
+
+export function setIsLoadingCityData(isLoadingCityData) {
+  return {
+    type: actionTypes.SET_IS_LOADING_CITY_DATA,
+    payload: isLoadingCityData,
+  };
+}
+
+export function setCityData(cityDate) {
+  return {
+    type: actionTypes.SET_CITY_DATA,
+    payload: cityDate,
+  };
+}
+
+export function toggleHighSpeed() {
+  return (dispatch, getState) => {
+    const { highSpeed } = getState();
+    dispatch({
+      type: actionTypes.SET_HIGH_SPEED,
+      payload: !highSpeed,
+    });
+  };
+}
+
+export function showCitySelector(currentSelectingLeftCity) {
+  return (dispatch) => {
+    dispatch({
+      type: actionTypes.SET_IS_CITY_SELECTOR_VISIBLE,
+      payload: true,
+    });
+
+    dispatch({
+      type: actionTypes.SET_CURRENT_SELECTING_LEFT_CITY,
+      payload: currentSelectingLeftCity,
+    });
+  };
+}
+
+export function hideCitySelector() {
+  return {
+    type: actionTypes.SET_IS_CITY_SELECTOR_VISIBLE,
+    payload: false,
+  };
+}
+
+export function setSelectedCity(city) {
+  return (dispatch, getState) => {
+    const { currentSelectingLeftCity } = getState();
+
+    if (currentSelectingLeftCity) {
+      dispatch(setFrom(city));
+    } else {
+      dispatch(setTo(city));
+    }
+
+    dispatch(hideCitySelector());
+  };
+}
+
+export function showDateSelector() {
+  return {
+    type: actionTypes.SET_IS_DATE_SELECTOR_VISIBLE,
+    payload: true,
+  };
+}
+
+export function hideDateSelector() {
+  return {
+    type: actionTypes.SET_IS_DATE_SELECTOR_VISIBLE,
+    payload: false,
+  };
+}
+
+export function exchangeFromTo() {
+  return (dispatch, getState) => {
+    const { from, to } = getState();
+    dispatch(setFrom(to));
+    dispatch(setTo(from));
+  };
+}
--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -1,5 +1,33 @@
-function reducer(state = {}) {
-  return state;
+import * as actionTypes from './action-types';
+
+const initState = {
+  from: '北京',
+  to: '上海',
+  isCitySelectorVisible: false,
+  currentSelectingLeftCity: false,
+  cityData: null,
+  isLoadingCityData: false,
+  isDateSelectorVisible: false,
+  departDate: Date.now(),
+  highSpeed: false,
+};
+
+function reducer(state = initState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case actionTypes.SET_FROM:
+    case actionTypes.SET_TO:
+    case actionTypes.SET_ISCITYSELECTORVISIBLE:
+    case actionTypes.SET_CURRENTSELECTINGLEFTCITY:
+    case actionTypes.SET_CITYDATA:
+    case actionTypes.SET_ISLOADINGCITYDATA:
+    case actionTypes.SET_ISDATESELECTORVISIBLE:
+    case actionTypes.SET_DEPARTDATE:
+    case actionTypes.SET_HIGHSPEED:
+      return payload;
+    default:
+      return state;
+  }
 }
 
 export default reducer;
