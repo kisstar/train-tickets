165430b6e4bb2b14f1e6679748ee054e8e6c6b28
kisstar <dwh.chn@foxmail.com>
2020-05-16 00:11:21 +0800

    feat: 构建主页的基本布局

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,6 +2,7 @@ import React, { useCallback } from 'react';
 import { connect } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import Header from '../../components/header';
+import './index.scss';
 
 function Home() {
   const history = useHistory();
@@ -9,7 +10,23 @@ function Home() {
     history.goBack();
   }, [history]);
 
-  return <Header title='火车票' onBack={onBack} />;
+  return (
+    <div className='home'>
+      <Header title='火车票' onBack={onBack} />
+      <div className='theme-image'></div>
+      <div className='search-container'></div>
+      <ul className='btn-list'>
+        <li className='btn-item'>
+          <span className='icon grab'></span>
+          <span className='font'>抢票</span>
+        </li>
+        <li className='btn-item'>
+          <span className='icon order'></span>
+          <span className='font'>我的订单</span>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 function mapStateToProps() {
