18285610d56e5038ad81d85d05e545480fcb6bda
kisstar <dwh.chn@foxmail.com>
2020-05-16 14:23:12 +0800

    feat: 添加 redux-logger 中间件

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,15 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import ReduxThunk from 'redux-thunk';
+import logger from 'redux-logger';
 import reducers from './reducers';
 
-const storeEnhancer = applyMiddleware(ReduxThunk);
+const middleWares = [ReduxThunk];
+
+if (process.env.NODE_ENV === 'development') {
+  middleWares.push(logger);
+}
+
+const storeEnhancer = applyMiddleware(...middleWares);
 const reducer = combineReducers(reducers);
 const storeEnhancerStoreCreator = storeEnhancer(createStore);
 const store = storeEnhancerStoreCreator(reducer);
