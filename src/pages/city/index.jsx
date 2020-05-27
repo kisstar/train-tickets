01f2be4d88078780be7b5cfc7526f40cbc66fa99
kisstar <dwh.chn@foxmail.com>
2020-05-27 23:22:44 +0800

    feat: 城市的异步加载

--- src/pages/city/index.jsx
+++ src/pages/city/index.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useGoBack, useShallowEqualSelector } from '../../lib/utils';
+import { setIsLoadingCityData, setCityData } from './store';
 import { Header } from '../../components';
-import { useGoBack } from '../../lib/utils';
 import './index.scss';
+
+const selectCityState = (state) => state.city;
 
 function City() {
   const goBack = useGoBack();
+  const state = useShallowEqualSelector(selectCityState);
+  const dispatch = useDispatch();
+
+  function getCityData() {
+    if (state.isLoadingCityData) {
+      return;
+    }
+
+    dispatch(setIsLoadingCityData(true));
+
+    fetch('/read/cities')
+      .then((res) => res.json())
+      .then((cityData) => dispatch(setCityData(cityData)))
+      .catch(() => dispatch(setCityData([])))
+      .finally(() => {
+        dispatch(setIsLoadingCityData(false));
+      });
+  }
+
+  useEffect(getCityData, [goBack]);
 
   return (
     <div className='city'>
