112b09bf459fdf2c9adf1ec405784fe919c7499a
kisstar <dwh.chn@foxmail.com>
2020-07-14 22:29:21 +0800

    fix: 在 train-list 顶层修改数据后，筛选浮层不会更新

--- src/pages/train-list/components/bottom/components/BottomModal.jsx
+++ src/pages/train-list/components/bottom/components/BottomModal.jsx
@@ -1,13 +1,244 @@
-import React, { memo } from 'react';
+import React, { memo, useState, useMemo, useReducer } from 'react';
+import PropTypes from 'prop-types';
+import ModalTitle from './ModalTitle';
+import Option from './option';
+import Slider from './slider';
+
+function checkedReducer(state, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case 'toggle':
+      const newState = { ...state };
+      if (payload in newState) {
+        delete newState[payload];
+      } else {
+        newState[payload] = true;
+      }
+      return newState;
+    case 'reset':
+      return {};
+    default:
+  }
+
+  return state;
+}
 
 const BottomModal = memo(function BottomModal(props) {
+  const {
+    ticketTypes,
+    trainTypes,
+    departStations,
+    arriveStations,
+    checkedTicketTypes,
+    checkedTrainTypes,
+    checkedDepartStations,
+    checkedArriveStations,
+    departTimeStart,
+    departTimeEnd,
+    arriveTimeStart,
+    arriveTimeEnd,
+    setCheckedTicketTypes,
+    setCheckedTrainTypes,
+    setCheckedDepartStations,
+    setCheckedArriveStations,
+    setDepartTimeStart,
+    setDepartTimeEnd,
+    setArriveTimeStart,
+    setArriveTimeEnd,
+    toggleIsFiltersVisible,
+  } = props;
+
+  const [localCheckedTicketTypes, localCheckedTicketTypesDispatch] = useReducer(
+    checkedReducer,
+    checkedTicketTypes,
+    (checkedTicketTypes) => {
+      return {
+        ...checkedTicketTypes,
+      };
+    }
+  );
+
+  const [localCheckedTrainTypes, localCheckedTrainTypesDispatch] = useReducer(
+    checkedReducer,
+    checkedTrainTypes,
+    (checkedTrainTypes) => {
+      return {
+        ...checkedTrainTypes,
+      };
+    }
+  );
+
+  const [
+    localCheckedDepartStations,
+    localCheckedDepartStationsDispatch,
+  ] = useReducer(
+    checkedReducer,
+    checkedDepartStations,
+    (checkedDepartStations) => {
+      return {
+        ...checkedDepartStations,
+      };
+    }
+  );
+
+  const [
+    localCheckedArriveStations,
+    localCheckedArriveStationsDispatch,
+  ] = useReducer(
+    checkedReducer,
+    checkedArriveStations,
+    (checkedArriveStations) => {
+      return {
+        ...checkedArriveStations,
+      };
+    }
+  );
+
+  const [localDepartTimeStart, setLocalDepartTimeStart] = useState(
+    () => departTimeStart
+  );
+  const [localDepartTimeEnd, setLocalDepartTimeEnd] = useState(
+    () => departTimeEnd
+  );
+  const [localArriveTimeStart, setLocalArriveTimeStart] = useState(
+    () => arriveTimeStart
+  );
+  const [localArriveTimeEnd, setLocalArriveTimeEnd] = useState(
+    () => arriveTimeEnd
+  );
+
+  const optionGroup = [
+    {
+      title: '坐席类型',
+      options: ticketTypes,
+      checkedMap: localCheckedTicketTypes,
+      dispatch: localCheckedTicketTypesDispatch,
+    },
+    {
+      title: '车次类型',
+      options: trainTypes,
+      checkedMap: localCheckedTrainTypes,
+      dispatch: localCheckedTrainTypesDispatch,
+    },
+    {
+      title: '出发车站',
+      options: departStations,
+      checkedMap: localCheckedDepartStations,
+      dispatch: localCheckedDepartStationsDispatch,
+    },
+    {
+      title: '到达车站',
+      options: arriveStations,
+      checkedMap: localCheckedArriveStations,
+      dispatch: localCheckedArriveStationsDispatch,
+    },
+  ];
+
+  const isResetDisabled = useMemo(() => {
+    return (
+      Object.keys(localCheckedTicketTypes).length === 0 &&
+      Object.keys(localCheckedTrainTypes).length === 0 &&
+      Object.keys(localCheckedDepartStations).length === 0 &&
+      Object.keys(localCheckedArriveStations).length === 0 &&
+      localDepartTimeStart === 0 &&
+      localDepartTimeEnd === 24 &&
+      localArriveTimeStart === 0 &&
+      localArriveTimeEnd === 24
+    );
+  }, [
+    localCheckedTicketTypes,
+    localCheckedTrainTypes,
+    localCheckedDepartStations,
+    localCheckedArriveStations,
+    localDepartTimeStart,
+    localDepartTimeEnd,
+    localArriveTimeStart,
+    localArriveTimeEnd,
+  ]);
+
+  // 只在按下确定按钮时才更新上级 store 中的状态
+  const sure = () => {
+    setCheckedTicketTypes(localCheckedTicketTypes);
+    setCheckedTrainTypes(localCheckedTrainTypes);
+    setCheckedDepartStations(localCheckedDepartStations);
+    setCheckedArriveStations(localCheckedArriveStations);
+    setDepartTimeStart(localDepartTimeStart);
+    setDepartTimeEnd(localDepartTimeEnd);
+    setArriveTimeStart(localArriveTimeStart);
+    setArriveTimeEnd(localArriveTimeEnd);
+    toggleIsFiltersVisible();
+  };
+
+  const reset = () => {
+    if (isResetDisabled) {
+      return;
+    }
+
+    localCheckedTicketTypesDispatch({ type: 'reset' });
+    localCheckedTrainTypesDispatch({ type: 'reset' });
+    localCheckedDepartStationsDispatch({ type: 'reset' });
+    localCheckedArriveStationsDispatch({ type: 'reset' });
+    setLocalDepartTimeStart(0);
+    setLocalDepartTimeEnd(24);
+    setLocalArriveTimeStart(0);
+    setLocalArriveTimeEnd(24);
+  };
+
   return (
     <div className='bottom-modal'>
       <div className='bottom-dialog'>
-        <div className='bottom-dialog-content'>{props.children}</div>
+        <div className='bottom-dialog-content'>
+          <ModalTitle
+            ok={sure}
+            cancel={reset}
+            isResetDisabled={isResetDisabled}
+          />
+          {optionGroup.map((group) => (
+            <Option {...group} key={group.title} />
+          ))}
+          <Slider
+            title='出发时间'
+            currentStartHours={localDepartTimeStart}
+            currentEndHours={localDepartTimeEnd}
+            onStartChanged={setLocalDepartTimeStart}
+            onEndChanged={setLocalDepartTimeEnd}
+          />
+          <Slider
+            title='到达时间'
+            currentStartHours={localArriveTimeStart}
+            currentEndHours={localArriveTimeEnd}
+            onStartChanged={setLocalArriveTimeStart}
+            onEndChanged={setLocalArriveTimeEnd}
+          />
+        </div>
       </div>
     </div>
   );
 });
+
+BottomModal.propTypes = {
+  ticketTypes: PropTypes.array.isRequired,
+  trainTypes: PropTypes.array.isRequired,
+  departStations: PropTypes.array.isRequired,
+  arriveStations: PropTypes.array.isRequired,
+  checkedTicketTypes: PropTypes.object.isRequired,
+  checkedTrainTypes: PropTypes.object.isRequired,
+  checkedDepartStations: PropTypes.object.isRequired,
+  checkedArriveStations: PropTypes.object.isRequired,
+  departTimeStart: PropTypes.number.isRequired,
+  departTimeEnd: PropTypes.number.isRequired,
+  arriveTimeStart: PropTypes.number.isRequired,
+  arriveTimeEnd: PropTypes.number.isRequired,
+  setCheckedTicketTypes: PropTypes.func.isRequired,
+  setCheckedTrainTypes: PropTypes.func.isRequired,
+  setCheckedDepartStations: PropTypes.func.isRequired,
+  setCheckedArriveStations: PropTypes.func.isRequired,
+  setDepartTimeStart: PropTypes.func.isRequired,
+  setDepartTimeEnd: PropTypes.func.isRequired,
+  setArriveTimeStart: PropTypes.func.isRequired,
+  setArriveTimeEnd: PropTypes.func.isRequired,
+  toggleIsFiltersVisible: PropTypes.func.isRequired,
+};
 
 export default BottomModal;
--- src/pages/train-list/components/bottom/index.jsx
+++ src/pages/train-list/components/bottom/index.jsx
@@ -1,32 +1,9 @@
-import React, { useState, useMemo, useReducer } from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { ORDER_DEPART } from '../../constant';
 import BottomModal from './components/BottomModal';
-import ModalTitle from './components/ModalTitle';
-import Option from './components/option';
-import Slider from './components/slider';
 import './index.scss';
-
-function checkedReducer(state, action) {
-  const { type, payload } = action;
-
-  switch (type) {
-    case 'toggle':
-      const newState = { ...state };
-      if (payload in newState) {
-        delete newState[payload];
-      } else {
-        newState[payload] = true;
-      }
-      return newState;
-    case 'reset':
-      return {};
-    default:
-  }
-
-  return state;
-}
 
 function Bottom({
   toggleOrderType,
@@ -59,92 +36,6 @@ function Bottom({
   setArriveTimeStart,
   setArriveTimeEnd,
 }) {
-  const [localCheckedTicketTypes, localCheckedTicketTypesDispatch] = useReducer(
-    checkedReducer,
-    checkedTicketTypes,
-    (checkedTicketTypes) => {
-      return {
-        ...checkedTicketTypes,
-      };
-    }
-  );
-
-  const [localCheckedTrainTypes, localCheckedTrainTypesDispatch] = useReducer(
-    checkedReducer,
-    checkedTrainTypes,
-    (checkedTrainTypes) => {
-      return {
-        ...checkedTrainTypes,
-      };
-    }
-  );
-
-  const [
-    localCheckedDepartStations,
-    localCheckedDepartStationsDispatch,
-  ] = useReducer(
-    checkedReducer,
-    checkedDepartStations,
-    (checkedDepartStations) => {
-      return {
-        ...checkedDepartStations,
-      };
-    }
-  );
-
-  const [
-    localCheckedArriveStations,
-    localCheckedArriveStationsDispatch,
-  ] = useReducer(
-    checkedReducer,
-    checkedArriveStations,
-    (checkedArriveStations) => {
-      return {
-        ...checkedArriveStations,
-      };
-    }
-  );
-
-  const [localDepartTimeStart, setLocalDepartTimeStart] = useState(
-    () => departTimeStart
-  );
-  const [localDepartTimeEnd, setLocalDepartTimeEnd] = useState(
-    () => departTimeEnd
-  );
-  const [localArriveTimeStart, setLocalArriveTimeStart] = useState(
-    () => arriveTimeStart
-  );
-  const [localArriveTimeEnd, setLocalArriveTimeEnd] = useState(
-    () => arriveTimeEnd
-  );
-
-  const optionGroup = [
-    {
-      title: '坐席类型',
-      options: ticketTypes,
-      checkedMap: localCheckedTicketTypes,
-      dispatch: localCheckedTicketTypesDispatch,
-    },
-    {
-      title: '车次类型',
-      options: trainTypes,
-      checkedMap: localCheckedTrainTypes,
-      dispatch: localCheckedTrainTypesDispatch,
-    },
-    {
-      title: '出发车站',
-      options: departStations,
-      checkedMap: localCheckedDepartStations,
-      dispatch: localCheckedDepartStationsDispatch,
-    },
-    {
-      title: '到达车站',
-      options: arriveStations,
-      checkedMap: localCheckedArriveStations,
-      dispatch: localCheckedArriveStationsDispatch,
-    },
-  ];
-
   const noChecked = useMemo(() => {
     return (
       Object.keys(checkedTicketTypes).length === 0 &&
@@ -166,56 +57,6 @@ function Bottom({
     arriveTimeStart,
     arriveTimeEnd,
   ]);
-
-  const isResetDisabled = useMemo(() => {
-    return (
-      Object.keys(localCheckedTicketTypes).length === 0 &&
-      Object.keys(localCheckedTrainTypes).length === 0 &&
-      Object.keys(localCheckedDepartStations).length === 0 &&
-      Object.keys(localCheckedArriveStations).length === 0 &&
-      localDepartTimeStart === 0 &&
-      localDepartTimeEnd === 24 &&
-      localArriveTimeStart === 0 &&
-      localArriveTimeEnd === 24
-    );
-  }, [
-    localCheckedTicketTypes,
-    localCheckedTrainTypes,
-    localCheckedDepartStations,
-    localCheckedArriveStations,
-    localDepartTimeStart,
-    localDepartTimeEnd,
-    localArriveTimeStart,
-    localArriveTimeEnd,
-  ]);
-
-  // 只在按下确定按钮时才更新上级 store 中的状态
-  const sure = () => {
-    setCheckedTicketTypes(localCheckedTicketTypes);
-    setCheckedTrainTypes(localCheckedTrainTypes);
-    setCheckedDepartStations(localCheckedDepartStations);
-    setCheckedArriveStations(localCheckedArriveStations);
-    setDepartTimeStart(localDepartTimeStart);
-    setDepartTimeEnd(localDepartTimeEnd);
-    setArriveTimeStart(localArriveTimeStart);
-    setArriveTimeEnd(localArriveTimeEnd);
-    toggleIsFiltersVisible();
-  };
-
-  const reset = () => {
-    if (isResetDisabled) {
-      return;
-    }
-
-    localCheckedTicketTypesDispatch({ type: 'reset' });
-    localCheckedTrainTypesDispatch({ type: 'reset' });
-    localCheckedDepartStationsDispatch({ type: 'reset' });
-    localCheckedArriveStationsDispatch({ type: 'reset' });
-    setLocalDepartTimeStart(0);
-    setLocalDepartTimeEnd(24);
-    setLocalArriveTimeStart(0);
-    setLocalArriveTimeEnd(24);
-  };
 
   return (
     <div className='bottom'>
@@ -249,30 +90,29 @@ function Bottom({
         </span>
       </div>
       {isFiltersVisible && (
-        <BottomModal>
-          <ModalTitle
-            ok={sure}
-            cancel={reset}
-            isResetDisabled={isResetDisabled}
-          />
-          {optionGroup.map((group) => (
-            <Option {...group} key={group.title} />
-          ))}
-          <Slider
-            title='出发时间'
-            currentStartHours={localDepartTimeStart}
-            currentEndHours={localDepartTimeEnd}
-            onStartChanged={setLocalDepartTimeStart}
-            onEndChanged={setLocalDepartTimeEnd}
-          />
-          <Slider
-            title='到达时间'
-            currentStartHours={localArriveTimeStart}
-            currentEndHours={localArriveTimeEnd}
-            onStartChanged={setLocalArriveTimeStart}
-            onEndChanged={setLocalArriveTimeEnd}
-          />
-        </BottomModal>
+        <BottomModal
+          ticketTypes={ticketTypes}
+          trainTypes={trainTypes}
+          departStations={departStations}
+          arriveStations={arriveStations}
+          checkedTicketTypes={checkedTicketTypes}
+          checkedTrainTypes={checkedTrainTypes}
+          checkedDepartStations={checkedDepartStations}
+          checkedArriveStations={checkedArriveStations}
+          departTimeStart={departTimeStart}
+          departTimeEnd={departTimeEnd}
+          arriveTimeStart={arriveTimeStart}
+          arriveTimeEnd={arriveTimeEnd}
+          setCheckedTicketTypes={setCheckedTicketTypes}
+          setCheckedTrainTypes={setCheckedTrainTypes}
+          setCheckedDepartStations={setCheckedDepartStations}
+          setCheckedArriveStations={setCheckedArriveStations}
+          setDepartTimeStart={setDepartTimeStart}
+          setDepartTimeEnd={setDepartTimeEnd}
+          setArriveTimeStart={setArriveTimeStart}
+          setArriveTimeEnd={setArriveTimeEnd}
+          toggleIsFiltersVisible={toggleIsFiltersVisible}
+        />
       )}
     </div>
   );
